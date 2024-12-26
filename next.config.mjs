cc4e1257349bd682899088a664b53c04f62bf705
Aryan-81 <131597580+Aryan-81@users.noreply.github.com>
2024-12-26 15:40:04 +0530

    --

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  // assetPrefix: '/portfolio-app', 
-  basePath: '/portfolio-app',  
+  // output: 'export',
+  // images: {
+  //   unoptimized: true,
+  // },
+  // // assetPrefix: '/portfolio-app', 
+  // basePath: '/portfolio-app',  
 };
 
 export default nextConfig;
